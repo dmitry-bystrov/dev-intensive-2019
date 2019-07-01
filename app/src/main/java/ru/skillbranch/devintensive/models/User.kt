d5456dd0d2870fc8299b47829c8c9336dc33cfa7
Дмитрий Быстров <bystrov@amigoweb.ru>
2019-07-01 16:44:28 +0900

    Паттерн Builder для класса User

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -14,7 +14,13 @@ data class User(
     var isOnline: Boolean = false
 ) {
 
-    constructor(id: String, firstName: String?, lastName: String?) : this(id, firstName, lastName, null)
+    constructor(id: String, firstName: String?, lastName: String?, lastVisit: Date? = Date()) : this(
+        id,
+        firstName,
+        lastName,
+        avatar = null,
+        lastVisit = lastVisit
+    )
 
     companion object Factory {
         var lastId = -1
@@ -24,5 +30,27 @@ data class User(
             val parts = Utils.parseFullName(fullName)
             return User("$lastId", parts.first, parts.second)
         }
+    }
+
+    class Builder {
+        private lateinit var id: String
+        private var firstName: String? = null
+        private var lastName: String? = null
+        private var avatar: String? = null
+        private var rating: Int = 0
+        private var respect: Int = 0
+        private var lastVisit: Date? = Date()
+        private var isOnline: Boolean = false
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 }
