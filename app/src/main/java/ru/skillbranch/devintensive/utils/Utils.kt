b2772edb8f958f4af41841c9b4f71c9cf2730bb3
Дмитрий Быстров <bystrov@amigoweb.ru>
2019-07-01 12:26:43 +0900

    Метод toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,11 +1,18 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
-    fun parseFullName(fullName: String?) : Pair<String?, String?> {
-        val parts : List<String>? = fullName?.split(" ")
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
         return (if (firstName.isNullOrEmpty()) null else firstName) to
                 (if (lastName.isNullOrEmpty()) null else lastName)
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        val initials = (if (firstName.isNullOrBlank()) "" else firstName.substring(0, 1)) +
+                if (lastName.isNullOrBlank()) "" else lastName.substring(0, 1)
+
+        return (if (initials.isEmpty()) null else initials.toUpperCase())
     }
 }
