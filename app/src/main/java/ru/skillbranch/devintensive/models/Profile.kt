cb92f11ea97d54e89ffb876384c66aacda53ef19
Дмитрий Быстров <bystrov@amigoweb.ru>
2019-07-30 09:45:28 +0900

    firstName + lastName = nickName

--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
+
 data class Profile(
     val firstName: String,
     val lastName: String,
@@ -8,7 +10,7 @@ data class Profile(
     val rating: Int = 0,
     val respect: Int = 0
 ) {
-    private val nickName: String = "John Doe" //TODO: hometask
+    private val nickName: String = Utils.transliteration("${firstName.trim()} ${lastName.trim()}", "_")
     private val rank: String = "Junior Android Developer"
 
     fun toMap(): Map<String, Any> = mapOf(
